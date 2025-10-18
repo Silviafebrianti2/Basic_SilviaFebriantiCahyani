6cab7c097208772994a3eebdd029c151ec29d08d
Silviafebrianti2 <silviafebrianti517@gmail.com>
2025-10-18 20:47:13 +0700

    comite1

---
+++ src/looping2/ForLoopNested.java
@@ -0,0 +1,20 @@
+package looping2;
+
+public class ForLoopNested {
+
+    public String getGrid(int max) {
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 1; i <= max; i++) {
+            for (int j = 1; j <= max; j++) {
+                result.append("Row ").append(i).append(" Col ").append(j);
+                // Tambahkan newline jika bukan data terakhir
+                if (!(i == max && j == max)) {
+                    result.append("\n");
+                }
+            }
+        }
+
+        return result.toString();
+    }
+}
